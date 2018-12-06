ae43c6755c62019047f274d5d6365ab8a2428e90
Vinnie Garcia <vinnie@coev.co>
2018-12-05 20:16:00 -0800

    feat (toolbar): skeleton of toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ToolbarComponent]
 })
 export class AppModule { }
